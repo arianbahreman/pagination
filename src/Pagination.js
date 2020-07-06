2cb6f55f01307e1dc975eca3ebb3447bcc82d2f0
Arian Bahreman <arian@Arians-MacBook-Air-2.local>
2020-07-06 17:53:21 +0430

    Pagination Init

---
+++ src/Pagination.js
@@ -0,0 +1,55 @@
+export default (current, limit, total, visibleNeighbors = 2) => {
+
+  let items = [];
+  const pages = Math.ceil(total / limit);
+
+  /*
+    Prev
+  */
+  if(current > 1)
+  {
+    items.push({type: 'prev', page: current -1});
+  }
+
+  /*
+    Pages
+  */
+  const pageStart = Math.max(1, current - visibleNeighbors),
+        pageEnd   = Math.min(pages, current + visibleNeighbors);
+
+  for(let index = pageStart; index <= pageEnd; index ++)
+  {
+    /*
+      Gap
+    */
+    if(index === pageStart && index - 1 > 1)
+    {
+      items.push({type: 'first', page: 1});
+      items.push({type: 'gap'});
+    }
+
+    /*
+      Anchor
+    */
+    items.push({type: 'anchor', page: index});
+
+    /*
+      Gap
+    */
+    if(index === pageEnd && pages - index > 1)
+    {
+      items.push({type: 'gap'});
+      items.push({type: 'last', page: pages});
+    }
+  }
+
+  /*
+    Next
+  */
+  if(current < pages)
+  {
+    items.push({type: 'next', page: current +1});
+  }
+
+  return items;
+}
